268a7a013488d63663743bd20be6158a81505d21
shreyansh-shrivastava05 <sshrivastava@osidigital.com>
2023-11-27 22:40:57 +0530

    Added the global Action Filter

---
+++ Employee Management/Filters/LowerCaseJsonResultFilter.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+using System.Text.Json.Nodes;
+
+namespace Employee_Management.Filters
+{
+    public class LowerCaseJsonResultFilter : ResultFilterAttribute
+    {
+        private object ConvertContentToLowerCase(object content)
+        {
+            if (content == null)
+            {
+                return null;
+            }
+
+            if (content is JObject jsonObject)
+            {
+                // Convert keys of the JSON object to lowercase
+                var properties = jsonObject.Properties().ToList();
+                foreach (var property in properties)
+                {
+                    var newPropertyName = property.Name.ToLower();
+                    if (newPropertyName != property.Name)
+                    {
+                        property.AddBeforeSelf(new JProperty(newPropertyName, property.Value));
+                        property.Remove();
+                    }
+                }
+
+                return jsonObject;
+            }
+
+            if (content is IEnumerable<object> enumerableContent)
+            {
+                // Recursively convert items in the enumerable
+                return enumerableContent.Select(item => ConvertContentToLowerCase(item)).ToList();
+            }
+
+            // For other types, return as is
+            return content;
+        }
+    }
+}
--- Employee Management/Program.cs
+++ Employee Management/Program.cs
@@ -1,9 +1,11 @@
+using Employee_Management.Filters;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-
+builder.Services.AddControllers(options => options.Filters.Add<LowerCaseJsonResultFilter>());
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
